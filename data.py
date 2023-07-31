88f3aeca7fe3f31879c4db942984f34d801cb76b
aki <aki@akis-MacBook-Air.local>
2023-08-01 07:29:29 +0900

    deploy test22

---
+++ data.py
@@ -0,0 +1,6 @@
+pdf_urls = [
+    # 'https://manual.buffalo.jp/buf-doc/35021178-39.pdf',
+    # 他のPDFのURLをここに追加
+    # 'https://www.ndl.go.jp/jp/aboutus/outline/pdf/pamph_ndl.pdf'
+    'https://www.caa.go.jp/law/bills/assets/representation_cms212_230417_01.pdf'
+]
