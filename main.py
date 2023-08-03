2401622f258d3a1e52a440e36acc6e7fa6d2aa00
aki <aki@akis-MacBook-Air.local>
2023-08-04 08:04:32 +0900

    deploy test27

--- main.py
+++ main.py
@@ -11,17 +11,18 @@ import sys
 from pypdf import PdfReader
 from transformers import GPT2TokenizerFast
 # from langchain.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.chains.question_answering import load_qa_chain
-from langchain.llms import OpenAI
-from langchain.chains import ConversationalRetrievalChain
+# from langchain.text_splitter import RecursiveCharacterTextSplitter
+# from langchain.embeddings import OpenAIEmbeddings
+# from langchain.vectorstores import FAISS
+# from langchain.chains.question_answering import load_qa_chain
+# from langchain.llms import OpenAI
+# from langchain.chains import ConversationalRetrievalChain
 from data import pdf_urls
 
 # ここにOpenAIから取得したキーを設定します。
 # os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
-
+print("Error no 1111")
+            sys.exit()
 app = FastAPI()
 
 @app.get("/")
